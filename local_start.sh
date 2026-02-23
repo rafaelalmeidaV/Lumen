d33480bc6a6366527148667bb6dd38bd4237684e
Bruno <bruno.3av@gmail.com>
2026-02-23 18:20:03 -0300

    feat: add local start script for services and keycloak

---
+++ local_start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+trap "kill 0" SIGINT
+
+PID=$(lsof -ti :8081)
+if [ -n "$PID" ]; then
+  kill -9 $PID
+fi
+
+(
+  cd ./candles-service/app || exit
+  go run ./cmd/api/main.go
+) &
+
+(
+  cd ./auth-service/app || exit
+  npx http-server . --cors -p 8081
+) &
+
+(
+  docker start keycloak 2>/dev/null || true
+) &
+
+wait
