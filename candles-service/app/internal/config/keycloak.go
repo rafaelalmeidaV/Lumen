eb2fe258b3a3f189caa5f1198a34bc9ab8cfd464
Bruno <bruno.3av@gmail.com>
2026-02-24 18:34:28 -0300

    feat: refactor Keycloak configuration to load from environment variables

--- candles-service/app/internal/config/keycloak.go
+++ candles-service/app/internal/config/keycloak.go
@@ -1,10 +1,20 @@
 package config
 
-const (
-	KeycloakBaseURL     = "http://lumen.auth.local"
-	KeycloakInternalURL = "http://auth-service-auth-chart.auth-service:8080"
+import "os"
 
-	Realm   = "Lumen"
-	Issuer  = KeycloakBaseURL + "/realms/" + Realm
-	JwksURL = KeycloakInternalURL + "/realms/" + Realm + "/protocol/openid-connect/certs"
+var (
+	KeycloakBaseURL     string
+	KeycloakInternalURL string
+	Realm   string
+	Issuer  string
+	JwksURL string
 )
+
+func LoadKeycloakConfig() {
+	Realm = os.Getenv("KEYCLOAK_REALM")
+	KeycloakBaseURL = os.Getenv("KEYCLOAK_BASE_URL")
+	KeycloakInternalURL = os.Getenv("KEYCLOAK_INTERNAL_URL")
+
+	Issuer = KeycloakBaseURL + "/realms/" + Realm
+	JwksURL = KeycloakInternalURL + "/realms/" + Realm + "/protocol/openid-connect/certs"
+}
