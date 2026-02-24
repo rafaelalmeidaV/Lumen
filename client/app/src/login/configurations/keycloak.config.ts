e0696e909854dbdfc73daf7563d98b763234e250
Bruno <bruno.3av@gmail.com>
2026-02-24 18:47:54 -0300

    feat: refactor Keycloak configuration to use environment variables

--- client/app/src/login/configurations/keycloak.config.ts
+++ client/app/src/login/configurations/keycloak.config.ts
@@ -1,11 +1,22 @@
 import type { KeycloakConfig } from '../types/Ikeycloak.config'
 
-export const CONFIG: KeycloakConfig = {
-  KEYCLOAK_BASE: "http://lumen.auth.local",
-  REALM: "Lumen",
-  CLIENT_ID: "public",
-  REDIRECT_URI: "http://lumen.client.local/"
+const requiredEnv = (key: string): string => {
+  const value = import.meta.env[key as keyof ImportMetaEnv]
+  if (!value) {
+    throw new Error(`Missing env variable: ${key}`)
+  }
+  return value as string
 }
 
-export const AUTH_ENDPOINT = `${CONFIG.KEYCLOAK_BASE}/realms/${CONFIG.REALM}/protocol/openid-connect/auth`
-export const TOKEN_ENDPOINT = `${CONFIG.KEYCLOAK_BASE}/realms/${CONFIG.REALM}/protocol/openid-connect/token`
+export const CONFIG: KeycloakConfig = {
+  KEYCLOAK_BASE: requiredEnv('VITE_KEYCLOAK_BASE_URL'),
+  REALM: requiredEnv('VITE_KEYCLOAK_REALM'),
+  CLIENT_ID: requiredEnv('VITE_CLIENT_ID'),
+  REDIRECT_URI: requiredEnv('VITE_KEYCLOAK_INTERNAL_URL')
+}
+
+export const AUTH_ENDPOINT =
+  `${CONFIG.KEYCLOAK_BASE}/realms/${CONFIG.REALM}/protocol/openid-connect/auth`
+
+export const TOKEN_ENDPOINT =
+  `${CONFIG.KEYCLOAK_BASE}/realms/${CONFIG.REALM}/protocol/openid-connect/token`
